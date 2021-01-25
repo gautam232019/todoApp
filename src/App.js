cd7abd8d7bcda625502b36c0c2ea397a7d80a687
Gautam Sachdeva <gautamsach20@gmail.com>
2021-01-25 12:38:33 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React,{useReducer} from "react"
+import {Container} from "reactstrap"
+import "bootstrap/dist/css/bootstrap.min.css"
+import "./App.css"
+import {TodoContext} from "./context/TodoContext"
+import TodoReducer from "./context/reducer"
+import TodoForm from "./components/TodoForm";
+import Todos from "./components/Todos"
+const App = () => {
+  const [todos,dispatch] = useReducer(TodoReducer,[])
+  return(
+    <TodoContext.Provider value={{todos,dispatch}}>
+    <Container fluid>
+      <h1>Todo App with context API</h1>
+      <Todos/>
+      <TodoForm/>
+    </Container>
+    </TodoContext.Provider>
+  )
+}
+
+export default App;
+
